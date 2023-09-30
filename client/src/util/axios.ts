aff72ad0a0d2af0196a1fb40ea4784fb94f09770
mukadas026 <mukadasm026@gmail.com>
2023-09-30 14:31:05 +0000

    add netlify redirects

--- client/src/util/axios.ts
+++ client/src/util/axios.ts
@@ -2,9 +2,9 @@ import axios from "axios"
 
 export const client = axios.create({
 	// production
-	// baseURL: "https://outerbase-hackathon-server.onrender.com/",
+	baseURL: "https://outerbase-hackathon-server.onrender.com/",
 	// dev
-	baseURL: "http://localhost:7000/",
+	// baseURL: "http://localhost:7000/",
 
 	headers: {
 		"Access-Control-Allow-Origin": "*",
